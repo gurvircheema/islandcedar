7746d71a72389a446394df03b43827abd0068630
Gurvir Cheema <gurvircheema@hotmail.com>
2016-07-12 22:56:05 -0700

    Initial commit

---
+++ source/javascripts/all.js
@@ -0,0 +1,11 @@
+// This is where it all goes :)
+
+$(document).ready(function(){
+  $('#menu-toggle').click(function(){
+    $('#sidebar-wrapper').toggleClass('active');
+  });
+
+  $('#menu-close').click(function(){
+    $('#sidebar-wrapper').toggleClass('active');
+  });
+});
